314a06acd2ab66ad314d2357a19da673ec52a329
Lxxn <ivxn.a.21@gmail.com>
2023-08-26 15:24:16 -0600

    Primera version

---
+++ src/main/java/com/lxxn/app/entity/User.java
@@ -0,0 +1,73 @@
+package com.lxxn.app.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = -3591162704489596402L;
+
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	private long id;
+	
+	@Column (length = 50)
+	private String name;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private boolean enabled;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	
+	
+}
---
+++ src/test/java/com/lxxn/app/CursoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxxn.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
